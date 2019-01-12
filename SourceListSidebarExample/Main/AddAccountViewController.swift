b5d6f9af79ef26b10f25f60451ffbd1825294175
Lukas Wolfsteiner <coding@dotwee.de>
2019-01-12 04:35:25 +0100

    Implement RxSwift to disable empty-username inputs

--- SourceListSidebarExample/Main/AddAccountViewController.swift
+++ SourceListSidebarExample/Main/AddAccountViewController.swift
@@ -6,11 +6,16 @@
 //  Copyright © 2019 Lukas Wolfsteiner. All rights reserved.
 //
 
+import RxSwift
+import RxCocoa
 import Cocoa
+import AppKit
 
 class AddAccountViewController: NSViewController {
 
     @IBOutlet weak var textFieldUsername: NSTextField!
+    
+    @IBOutlet weak var buttonOk: NSButton!
     
     @IBOutlet weak var popUpButtonProvider: NSPopUpButton!
     
@@ -25,14 +30,10 @@ class AddAccountViewController: NSViewController {
         let username = textFieldUsername.stringValue
         let provider = ProviderManager.PROVIDERS_LIST[popUpButtonProvider.indexOfSelectedItem]
         
-        if !username.isEmpty {
-            let account = accountsManager.addAccount(username: username, provider: provider)
-            
-            print("NewAccountViewController: Created account=\(account) with values username=\(username) provider=\(provider)")
-            self.dismiss(self)
-        } else {
-            print("NewAccountViewController: Error username=\(username) provider=\(provider) must both be not empty!")
-        }
+        let account = accountsManager.addAccount(username: username, provider: provider)
+        
+        print("NewAccountViewController: Created account=\(account) with values username=\(username) provider=\(provider)")
+        self.dismiss(self)
     }
     
     @IBAction func onButtonCancelAction(_ sender: NSButton) {
@@ -44,6 +45,14 @@ class AddAccountViewController: NSViewController {
         
         // Do view setup here.
         self.popUpButtonProvider.addItems(withTitles: self.items)
+        self.textFieldUsername
+            .rx.text
+            .distinctUntilChanged()
+            .subscribe(onNext: { (usernameValue) in
+                print("onNext: \(String(describing: usernameValue))")
+                
+                self.buttonOk.isEnabled = usernameValue != nil && !(usernameValue?.isEmpty)!
+            })
     }
     
 }
